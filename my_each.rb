1eeb5b00412669565dacf237543147266fb4d97c
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-18 11:04:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array)# put argument(s) here
   # code here
+  num = 0
+  alen = array.length
+  while num < alen
+    yield array[num]
+    num += 1
+  end
+  array
+
 end
